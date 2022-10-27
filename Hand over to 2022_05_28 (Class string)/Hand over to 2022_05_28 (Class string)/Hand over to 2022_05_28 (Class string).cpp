9e858adc585f7669ffa8394a9ef9cd3b3164e714
NatalyaBah <104036796+NatalyaBah@users.noreply.github.com>
2022-10-28 00:42:47 +0300

    Add files via upload

---
+++ Hand over to 2022_05_28 (Class string)/Hand over to 2022_05_28 (Class string)/Hand over to 2022_05_28 (Class string).cpp
@@ -0,0 +1,317 @@
+﻿#include <iostream>
+#include "String.h"
+using namespace std;
+
+int main()
+{    
+    char* string1 = new char[]{"Hello! How are you?"};
+    char* string2 = new char[] {"Thanks! And you?"};
+    String str(string1);
+    String str2(string2);
+    str.print();
+    cout << endl;
+    cout << "Line size = " << str.size() << endl;;
+    /*int n;
+    cout << "From how many elements to output a new line : ";
+    cin >> n;
+    if(str.size() >=n) str.reSize(n);
+    else
+    {
+        char c;
+        cout << "Input symbol you wish to add to your string: ";
+        cin >> c;
+        str.reSize(n, c);
+    }
+    cout << endl;   
+    str.print();*/
+
+  /*  str.clear();
+    cout << endl << "The size of your string is: " << str.size();
+    str.print();*/
+
+  /*  cout << "0 - your string is full, 1 - string is empty: " << str.empty();
+    str.print();*/
+
+   /* char a;
+    cout << "Enter character to add";
+    str.push_back(a);
+    str.print();*/
+
+   /* cout << "What do you wish to do with your string?" << endl;
+    cout << "Input:"
+        "\n - \"1\" if you want to add some similiar symbols;"
+        "\n - \"2\" if you want to add some string;"
+        "\n - \"3\" if you want to add several symbols of some string beginning from index." << endl;
+    int q;
+    cout << "Choose one of the variants (1-3): ";
+    cin >> q;
+    switch (q)
+    {
+    case 1:
+        int n;
+        char b;
+        cout << "Input symbol you wish to add to the string: ";
+        cin >> b;
+        cout << "Input the amount of symbols: ";
+        cin >> n;
+        str.append(n, b);
+        str.print();
+        break;
+    case 2:
+        str.append(str2);
+        str.print();
+        break;
+    case 3:
+        int index;
+        int quant;
+        cout << "Input index, which you want to copy the second string from (from 0 to " << str2.size() << "): ";
+        cin >> index;
+        cout << "Input quantity, how many elements you want to copy from the second string (max: " << str2.size() - index << "): ";
+        cin >> quant;
+        str.append(str2, index, quant);
+        str.print();
+        break;
+    }*/
+
+    /*cout << "What do you wish to do with your string?" << endl;
+    cout << "Input:"
+        "\n - \"1\" if you want to delete last elements from index;"
+        "\n - \"2\" if you want to delete some elements." << endl;
+    int q;
+    int index;
+    cout << "Choose one of the variants (1-2): ";
+    cin >> q;
+    switch (q)
+    {
+    case 1:
+        cout << "Input index you want to delete your string from (from 0 to " << str.size() << "): ";
+        cin >> index;
+        str.erase(index);
+        cout << endl << "Your new string:";
+        str.print();
+        break;
+
+    case 2:
+        cout << "Input index you want to delete your string from (from 0 to " << str.size() << "): ";
+        cin >> index;
+        int quant;
+        cout << "Input quantity of symbols you wish to delete: ";
+        cin >> quant;
+        str.erase(index, quant);
+        str.print();
+        break;
+    }*/
+
+    /*int index, index2, quant;
+    char symbol;
+    cout << "What do you wish to do with your string?" << endl;
+    cout << "Input:"
+        "\n - \"1\" if you want to input several similiar symbols;"
+        "\n - \"2\" if you want to input another string from any symbol of the first string;"
+        "\n - \"3\" if you want to input several symbols of another string;" << endl;
+    int q;
+    cout << "Choose one of the variants (1-3): ";
+    cin >> q;
+    switch (q)
+    {
+        case 1:
+            cout << "Input index you want to input your symbol from (from 0 to " << str.size() << "): ";
+            cin >> index;
+            cout << "Input quantity of symbols you wish to add: ";
+            cin >> quant;
+            cout << "Input symbol you wish to add: ";
+            cin >> symbol;
+            str.insert(index, quant, symbol);
+            str.print();
+            break;
+        case 2:
+            cout << "Input index you want to input your string from (from 0 to " << str.size() << "): ";
+            cin >> index;
+            str.insert(str2, index);
+            str.print();
+            break;
+        case 3:
+            cout << "Input index you want to input your string from (from 0 to " << str.size() << "): ";
+            cin >> index;
+            cout << "Input index you want to copy another string from (from 0 to " << str2.size() << "): ";
+            cin >> index2;
+            cout << "Input how many elements you wish to copy from another string: ";
+            cin >> quant;
+            str.insert(str2, index, index2, quant);
+            str.print();
+            break;
+    }*/
+    
+     /*   String str3;
+        int index, q;
+
+        cout << "What do you wish to do with your string?" << endl;
+        cout << "Input:"
+        "\n - \"1\" if you want to get substring beginning from index and to the end;"
+        "\n - \"2\" if you want to get substring beginning from index to some quantity." << endl;
+        cin >> q;
+        switch (q)
+        {
+        case 1:
+            cout << "Input index you want to make new string (from 0 to " << str.size() << "): ";
+            cin >> index;
+
+            str3.substr(str, index);
+            str3.print();
+            break;
+        case 2:
+            int quant;
+            cout << "Input index you want to make new string (from 0 to " << str.size() << "): ";
+            cin >> index;
+            cout << "Input quantity of symbols you wish to make new string: ";
+            cin >> quant;
+            str3.substr(str, index, quant);
+            str3.print();
+            break;
+        }*/
+
+    /*int q;
+    cout << "What do you wish to do with your string?" << endl;
+    cout << "Input:"
+    "\n - \"1\" if you want to get string with some replaced symbols by entered symbol;"
+    "\n - \"2\" if you want to get string with some replaced symbols by another string;"
+    "\n - \"3\" if you want to get string with some replaced symbols by another string from some index and quantity of symbols." << endl;
+    cin >> q;
+    int index, quant1;
+    switch (q)
+    {
+    case 1:
+        int quant2;
+        char s;
+        cout << "Input index you wish to replace symbols (from 0 to " << str.size() << ") : ";
+        cin >> index;
+        cout << "Input quantity of symbols you wish to replace: ";
+        cin >> quant1;
+        cout << "Input symbol you wish to be placed: ";
+        cin >> s;
+        cout << "Input quantity of symbols you wish to be placed: ";
+        cin >> quant2;
+        str.replace(index, quant1, quant2, s);
+        str.print();
+        break;
+    case 2:
+        cout << "Input index you wish to replace symbols (from 0 to " << str.size() << ") : ";
+        cin >> index;
+        cout << "Input quantity of symbols you wish to replace: ";
+        cin >> quant1;
+        str.replace(index, quant1, str2);
+        str.print();
+    case 3:
+        int index2, quant3;
+        cout << "Input index you wish to replace symbols (from 0 to " << str.size() << ") : ";
+        cin >> index;
+        cout << "Input quantity of symbols you wish to replace: ";
+        cin >> quant1;
+        cout << "Input index you wish to replace symbols (from 0 to " << str2.size() << ") : ";
+        cin >> index2;
+        cout << "Input quantity of symbols you wish to take from the second string: ";
+        cin >> quant3;
+        str.replace(index, quant1, str2, index2, quant3);
+        str.print();
+    }*/
+
+    /*char* string4 = new char[] {"you"};
+    String str4(string4);
+
+    int q;
+    int index = 0;
+    cout << "What do you wish to find?" << endl;
+    cout << "Input:"
+    "\n - \"1\" if you want to get index of the first similiar symbol;"
+    "\n - \"2\" if you want to get index of the first several similiar symbols." << endl;
+    cin >> q;
+    switch (q)
+    {
+    case 1:
+        cout << "Searching for: " << endl;
+        str4.print();
+        cout << "If you wish to search from some index, please input value (from 0 to " << str.size() << "): ";
+        cin >> index;
+        cout << "Result of searching is: " << str.find(str4, index);
+    case 2:
+        int quant;
+        cout << "Searching for: " << endl;
+        str4.print();
+        cout << "Input index you want to search (from 0 to " << str.size() << "): ";
+        cin >> index;
+        cout << "Input quantity if symbols you wish to search: ";
+        cin >> quant;
+        cout << "Result of searching is: " << str.find(str4, index, quant);
+    }*/
+
+ /*   char* string5 = new char[] {"are"};
+    String str5(string5);
+    int index, q;
+    cout << "What do you wish to find?" << endl;
+    cout << "Input:"
+    "\n - \"1\" if you want to get index of the first similiar symbol;"
+    "\n - \"2\" if you want to get index of the first several similiar symbols." << endl;
+    cin >> q;
+    switch (q)
+    {
+    case 1:
+        cout << "Searching for: " << endl;
+        str5.print();
+        cout << "If you wish to search from some index, please input value (from 0 to " << str.size() << "): ";
+        cin >> index;
+        cout << "Result of searching is: " << str.rfind(str5, index) << endl;
+        break;
+    case 2:
+        cout << "Searching for: " << endl;
+        str5.print();
+        cout << "If you wish to search from some index, please input value (from 0 to " << str.size() << "): ";
+        cin >> index;
+        int quant;
+        cout << "How many symbols you wish to find: ";
+        cin >> quant;
+        cout << "Result of searching is: " << str.rfind(str5, index, quant) << endl;
+        break;
+    }*/
+
+    //char* string6 = new char[] {"you?"};
+    //String str6(string6);
+    //int index;
+    //cout << "Searching for the index of the first symbol from string: ";
+    //str6.print();
+    //cout << "Please input index (from 0 to " << str.size() - 1 << "), if you wish to search from some index: ";
+    //cin >> index;
+    //cout << endl << "First index of the same symbol in the original and searching strings: " << str.find_first_of(str6, index);
+
+    /*char* string7 = new char[] {"Why?"};
+    String str7(string7);
+    int index;
+    cout << "Searching for the index of the first symbol from string: ";
+    str7.print();
+    cout << "Please input index (from 0 to " << str.size() - 1 << "), if you wish to search from some index: ";
+    cin >> index;
+    cout << endl << "Last index of the same symbol in the original and searching strings: " << str.find_last_of(str7, index);*/
+
+    
+
+   /* char* string8 = new char[] {"Hello! How is Tom?"};
+    String str8(string8);
+    int index;
+    cout << "Searching for the index of the first mismatched symbol from string: ";
+    str8.print();
+    cout << "Please input index (from 0 to " << str.size() - 1 << "), if you wish to search from some index: ";
+    cin >> index;
+    cout << endl << "First index of different symbol in the original string from another: " << str.find_first_not_of(str8, index);*/
+
+    /*char* string9 = new char[] {"Are you thinking?"};
+    String str9(string9);
+    int index;
+
+    cout << "Searching for the index of the first mismatched symbol from string: ";
+    str9.print();
+    cout << "Please input index (from 0 to " << str.size() - 1 << "), if you wish to search from some index: ";
+    cin >> index;
+    cout << endl << "Last index of different symbol in the original string from another: " << str.find_last_not_of(str9, index);*/
+
+   /* cout << "Your first string is: " << str.c_str() << endl;
+    cout << "Your second string is: " << str2.c_str() << endl;*/
+}
---
+++ Hand over to 2022_05_28 (Class string)/Hand over to 2022_05_28 (Class string)/String.h
@@ -0,0 +1,588 @@
+﻿#pragma once
+using namespace std;
+class String
+{
+private:
+	int sizeStr;
+	char* str = new char[sizeStr];
+	int checkIndex(int index, int size)
+	{
+		if (index > size) return size;
+		else return index;
+	}
+
+	int checkQuant(int index, int quant, int size)
+	{
+		if (index + quant > size) return size - index;
+		else return quant;
+	}
+public:
+	String() :str(nullptr)
+	{
+
+	}
+	String(int sizeStr) :sizeStr(NULL)
+	{
+		this->sizeStr = sizeStr;
+	}
+	String(char* str)
+	{
+		this->str = str;
+	}
+	~String()
+	{
+		delete[] this->str;
+	}
+
+	int size()
+	{
+		for (int i = 0; str[i] != '\0'; i++)
+		{
+			sizeStr++;
+		}
+		return sizeStr;
+	}
+
+	void reSize(int n)
+	{
+		char* buf = new char[n + 1];
+		for (int i = 0; i < n; i++)
+		{
+			buf[i] = str[i];
+		}
+		buf[n] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void reSize(int n, char c)
+	{
+		char* buf = new char[n + 1];
+		for (int i = 0; i < n; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = 0; i < n; i++)
+		{
+			buf[i] = c;
+		}
+		buf[n] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void print()
+	{
+		cout << str;
+	}
+
+	void clear()
+	{
+		char* buf = new char[1]{ "" };
+		buf[0] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	bool empty()
+	{
+
+		if (sizeStr == 0) return true;
+		else return false;
+	}
+
+	void push_back(char st)
+	{
+		int len = size();
+		char* buf = new char[sizeStr + 1];
+		for (int i = 0; i < sizeStr; i++)
+		{
+			buf[i] = str[i];
+		}
+		buf[sizeStr] = st;
+		delete[]str;
+		str = buf;
+	}
+
+	void append(int w, char s)
+	{
+		char* buf = new char[sizeStr + 1 + w];
+		for (int i = 0; i < sizeStr; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = sizeStr; i < sizeStr + w; i++)
+		{
+			buf[i] = s;
+		}
+		buf[sizeStr + w] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void append(String& str2)
+	{
+		int sizeStr2 = 0;
+		char* buf = new char[sizeStr + sizeStr2 + 1];
+		for (int i = 0; i < sizeStr; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = sizeStr, k = 0; i < sizeStr + sizeStr2; i++)
+		{
+			buf[i] = str2.str[k++];
+		}
+		buf[sizeStr + sizeStr2] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void append(String& str2, int index, int quant)
+	{
+		int sizeStr2 = 0;
+		index = str2.checkIndex(index, sizeStr);
+		quant = str2.checkQuant(index, quant, sizeStr2);
+
+		char* buf = new char[sizeStr + quant + 1];
+		for (int i = 0; i < sizeStr; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = sizeStr, k = index; i < sizeStr + quant; i++)
+		{
+			buf[i] = str2.str[k++];
+		}
+		buf[sizeStr + quant] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void erase(int index)
+	{
+		int sizeStr = size();
+		index = checkIndex(index, sizeStr);
+
+		char* buf = new char[index + 1];
+		for (int i = 0; i < index; i++)
+		{
+			buf[i] = str[i];
+		}
+		delete[]str;
+		str = buf;
+		str[index] = '\0';
+		sizeStr = index;
+	}
+
+	void erase(int index, int quant)
+	{
+
+		int sizeStr = size();
+		index = checkIndex(index, sizeStr);
+		quant = checkQuant(index, quant, sizeStr);
+		char* buf = new char[sizeStr - quant + 1];
+
+		for (int i = 0; i < index; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = index + quant, k = index; i < sizeStr; i++)
+		{
+			buf[k++] = str[i];
+		}
+		buf[sizeStr - quant] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void insert(int index, int quant, char symbol)
+	{
+		int sizeStr = size();
+		index = checkIndex(index, sizeStr);
+		char* buf = new char[sizeStr + quant + 1];
+		for (int i = 0; i < index; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = 0, k = index; i < quant; i++)
+		{
+			buf[k++] = symbol;
+		}
+		for (int i = index + quant, k = index; i < sizeStr + quant; i++)
+		{
+			buf[i] = str[k++];
+		}
+		buf[sizeStr + quant] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void insert(String& str2, int index)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str2.size();
+		index = checkIndex(index, sizeStr);
+
+		char* buf = new char[sizeStr + sizeStr2 + 1];
+		for (int i = 0; i < index; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = index, k = 0; i < index + sizeStr2; i++)
+		{
+			buf[i] = str2.str[k++];
+		}
+		for (int i = index + sizeStr2, l = index; i < sizeStr + sizeStr2; i++)
+		{
+			buf[i] = str[l++];
+		}
+		buf[sizeStr + sizeStr2] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void insert(String& str2, int index, int index2, int quant)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str2.size();
+		index = checkIndex(index, sizeStr);
+		index2 = checkIndex(index2, sizeStr2);
+		quant = checkQuant(index2, quant, sizeStr2);
+
+		char* buf = new char[sizeStr + quant + 1];
+
+		for (int i = 0; i < index; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = index2, k = index; i < index2 + quant; i++)
+		{
+			buf[k++] = str2.str[i];
+		}
+		for (int i = index, l = index + quant; i < sizeStr; i++)
+		{
+			buf[l++] = str[i];
+		}
+		buf[sizeStr + quant] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	String substr(String& str1, int index)
+	{
+		int sizeStr = str1.size();
+		index = checkIndex(index, sizeStr);
+		char* buf = new char[sizeStr - index + 1];
+		for (int i = index, k = 0; i < sizeStr; i++)
+		{
+			buf[k++] = str1.str[i];
+		}
+		buf[sizeStr - index] = '\0';
+		str = buf;
+		return str;
+	}
+
+	String substr(String& str1, int index, int quant)
+	{
+		int sizeStr = str1.size();
+		index = checkIndex(index, sizeStr);
+		quant = checkQuant(index, quant, sizeStr);
+		char* buf = new char[quant + 1];
+
+		for (int i = 0, k = index; i < quant; i++)
+		{
+			buf[i] = str1.str[k++];
+		}
+		buf[quant] = '\0';
+		str = buf;
+		return str;
+	}
+
+	void replace(int index, int quant1, int quant2, int symbol)
+	{
+		int sizeStr = size();
+		index = checkIndex(index, sizeStr);
+		quant1 = checkQuant(index, quant1, sizeStr);
+
+		char* buf = new char[sizeStr - quant1 + quant2 + 1];
+		for (int i = 0; i < index; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = 0, k = index; i < quant2; i++)
+		{
+			buf[k++] = symbol;
+		}
+		for (int i = index + quant1, k = index + quant2; i < sizeStr; i++)
+		{
+			buf[k++] = str[i];
+		}
+		buf[sizeStr - quant1 + quant2] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void replace(int index, int quant, String& str2)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str2.size();
+		index = checkIndex(index, sizeStr);
+		quant = checkQuant(index, quant, sizeStr);
+
+		char* buf = new char[sizeStr - quant + sizeStr2 + 1];
+
+		for (int i = 0; i < index; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = 0, k = index; i < sizeStr2; i++)
+		{
+			buf[k++] = str2.str[i];
+		}
+		for (int i = index + quant, k = index + sizeStr2; i < sizeStr; i++)
+		{
+			buf[k++] = str[i];
+		}
+		buf[sizeStr - quant + sizeStr2] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	void replace(int index, int quant1, String& str2, int index2, int quant3)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str2.size();
+		index = checkIndex(index, sizeStr);
+		quant1 = checkQuant(index, quant1, sizeStr);
+		index2 = checkIndex(index2, sizeStr2);
+		quant3 = checkQuant(index2, quant3, sizeStr2);
+
+		char* buf = new char[sizeStr - quant1 + quant3 + 1];
+
+		for (int i = 0; i < index; i++)
+		{
+			buf[i] = str[i];
+		}
+		for (int i = 0, k = index, l = index2; i < quant3; i++)
+		{
+			buf[k++] = str2.str[l++];
+		}
+		for (int i = index + quant1, k = index2 + quant3; i < sizeStr; i++)
+		{
+			buf[k++] = str[i];
+		}
+		buf[sizeStr - quant1 + quant3] = '\0';
+		delete[]str;
+		str = buf;
+	}
+
+	int find(String& str4, int index = 0)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str4.size();
+		bool flag;
+
+		index = checkIndex(index, sizeStr);
+
+		for (int i = 0; i < sizeStr; i++)
+		{
+			if (str[i] == str4.str[0])
+			{
+				flag = true;
+				for (int j = 1; j < sizeStr2; j++)
+				{
+					if (str[i + j] != str4.str[j])
+					{
+						flag = false;
+						break;
+					}
+				}
+				if (flag == true) return i;
+			}
+		}
+		return -1;
+	}
+
+	int find(String& str4, int index, int quant)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str4.size();
+		bool flag;
+
+		index = checkIndex(index, sizeStr);
+
+		if (quant > sizeStr2) quant = sizeStr2;
+		if (quant < 0) quant = 0;
+
+		for (int i = index; i < sizeStr; i++)
+		{
+			if (str[i] == str4.str[0])
+			{
+				flag = true;
+				for (int j = 1; j < sizeStr2; j++)
+				{
+					if (str[i + j] != str4.str[j])
+					{
+						flag = false;
+						break;
+					}
+				}
+				if (flag == true) return i;
+			}
+		}
+		return -1;
+	}
+
+	int rfind(String& str5, int index = 0)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str5.size();
+		bool flag;
+		index = sizeStr - index; //×òîáû ïîéòè ñ êîíöà ñòðîêè
+
+		for (int i = index; i >= 0; i--)
+		{
+			if (str[i] == str5.str[0])
+			{
+				flag = true;
+				for (int j = 1; j < sizeStr2; j++)
+				{
+					if (str[i + j] != str5.str[j])
+					{
+						flag = false;
+						break;
+					}
+				}
+				if (flag == true) return i;
+			}
+		}
+		return -1;
+	}
+
+	int rfind(String& str5, int index, int quant)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str5.size();
+		bool flag;
+
+		index = sizeStr - index;
+
+		if (index == sizeStr) index--;
+
+		if (quant > sizeStr2) quant = sizeStr2;
+		if (quant < 0) quant = 0;
+
+		for (int i = index; i >= 0; i--)
+		{
+			if (str[i] == str5.str[0])
+			{
+				flag = true;
+				for (int j = 1; j < quant; j++)
+				{
+					if (str[i + j] != str5.str[j])
+					{
+						flag = false;
+						break;
+					}
+				}
+				if (flag == true) return i;
+			}
+		}
+		return -1;
+	}
+
+	int find_first_of(String& str6, int index = 0)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str6.size();
+
+		index = checkIndex(index, sizeStr);
+
+		for (int i = index; i < sizeStr; i++)
+		{
+			for (int j = 0; j < sizeStr2; j++)
+			{
+				if (str[i] == str6.str[j])
+				{
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+	int find_last_of(String& str, int index = 0)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str.size();
+
+		if (index < 0) index = 0;
+
+		index = checkIndex(index, sizeStr);
+
+		for (int i = sizeStr - 1; i >= index; i--)
+		{
+			for (int j = 0; j < sizeStr2; j++)
+			{
+				if (str.str[i] == str.str[j]) return i;
+			}
+		}
+		return -1;
+	}
+
+	int find_first_not_of(String& str, int index = 0)
+	{
+		int sizeStr = size();
+		int sizeStr2 = str.size();
+		bool flag;
+
+		index = checkIndex(index, sizeStr);
+
+		for (int i = index; i < sizeStr; i++)
+		{
+			flag = true;
+			for (int j = 0; j < sizeStr2; j++)
+			{
+				if (str.str[i] == str.str[j])
+				{
+					flag = false;
+					break;
+				}
+			}
+			if (flag) return i;
+		}
+		return -1;
+	}
+
+	int find_last_not_of(String& str, int index = 0)
+	{
+		int sizeStr = size();
+		int sizeSt2 = str.size();
+		bool flag;
+
+		if (index < 0) index = 0;
+
+		index = checkIndex(index, sizeStr);
+
+		for (int i = sizeStr - 1; i >= 0; i--)
+		{
+			flag = true;
+			for (int j = 0; j < sizeStr; j++)
+			{
+				if (str.str[i] == str.str[j])
+				{
+					flag = false;
+					break;
+				}
+			}
+			if (flag) return i;
+		}
+		return -1;
+	}
+
+	char* c_str()
+	{
+		return str;
+	}
+
+};
+
